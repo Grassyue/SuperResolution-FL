6cd8cde2a09702a29f0a5e214a0b39c27bb2fbba
Grassyue <grass.yue@outlook.com>
2021-04-19 20:09:29 +0800

    commit

--- solvers/SRSolver.py
+++ solvers/SRSolver.py
@@ -16,15 +16,15 @@ from utils import util
 
 ############################################################
 ### Charbonnier Loss (L1)
-class CharbonnierLoss(nn.Module):
-    def __init__(self, eps=1e-3):
-        super(CharbonnierLoss, self).__init__()
-        self.eps = eps
+# class CharbonnierLoss(nn.Module):
+#     def __init__(self, eps=1e-3):
+#         super(CharbonnierLoss, self).__init__()
+#         self.eps = eps
 
-    def forward(self, x, y):
-        diff = x - y
-        loss = torch.mean(torch.sqrt((diff * diff) + (self.eps * self.eps)))
-        return loss
+#     def forward(self, x, y):
+#         diff = x - y
+#         loss = torch.mean(torch.sqrt((diff * diff) + (self.eps * self.eps)))
+#         return loss
 ############################################################
 
 
